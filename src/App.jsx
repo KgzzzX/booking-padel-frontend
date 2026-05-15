b3c43e8f3a490b7c230dac38ea652377e5cb0975
Aji Kartiko H. <aji45802@gmail.com>
2026-05-15 16:47:29 +0700

    setup Tailwind dan struktur projek

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="bg-black min-h-screen text-white flex items-center justify-center">
+      <h1 className="text-4xl font-bold">
+        Booking Padel Frontend
+      </h1>
+    </div>
+  )
+}
+
+export default App
